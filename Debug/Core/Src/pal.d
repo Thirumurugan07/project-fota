ffb2dbf1c15f66495204ed90c4adf5a83ff732b3
Thirumurugan07 <thirumurugans961@gmail.com>
2025-08-05 20:17:26 +0530

    Remove tracked files now ignored by .gitignore

--- Debug/Core/Src/pal.d
+++
@@ -1,4 +0,0 @@
-Core/Src/pal.o: ../Core/Src/pal.c ../optiga/include/optiga/pal/pal.h \
- ../optiga/include/optiga/common/optiga_lib_types.h
-../optiga/include/optiga/pal/pal.h:
-../optiga/include/optiga/common/optiga_lib_types.h:
--- Debug/Core/Src/pal_crypt_stub.d
+++
@@ -1,1 +0,0 @@
-Core/Src/pal_crypt_stub.o: ../Core/Src/pal_crypt_stub.c
