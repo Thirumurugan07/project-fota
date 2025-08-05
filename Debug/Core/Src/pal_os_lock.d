95fe5058327bfd07d9f2167a0a1a29bc40b82341
Thirumurugan07 <thirumurugans961@gmail.com>
2025-08-05 20:21:45 +0530

    chore: remove ignored files from version control

--- Debug/Core/Src/pal_os_lock.d
+++
@@ -1,7 +0,0 @@
-Core/Src/pal_os_lock.o: ../Core/Src/pal_os_lock.c \
- ../optiga/include/optiga/pal/pal_os_lock.h \
- ../optiga/include/optiga/pal/pal.h \
- ../optiga/include/optiga/common/optiga_lib_types.h
-../optiga/include/optiga/pal/pal_os_lock.h:
-../optiga/include/optiga/pal/pal.h:
-../optiga/include/optiga/common/optiga_lib_types.h:
